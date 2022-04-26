c7c810a9378fd011429fe0419ee750c2a7d9a8f2
Kelelchi Nwandu <nwandukelechi@gmail.com>
2022-04-26 23:57:00 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import './App.css';
-import React, { useState } from 'react';
-
 import Input from './Components/Input';
 import Modal from 'react-modal';
 
+Modal.setAppElement('#root')
+
 function App() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  
   return (
     <>
       <Input />
-      <button onClick={
-        () => setModalIsOpen(true)
-      }>Open Modal</button>
-      <Modal isOpen={modalIsOpen} onRequestClose={
-        () => setModalIsOpen(false)
-      }>
-        <h2>Modal Title</h2>
-        <h2>Modal Body</h2>
-        <button onClick={
-        () => setModalIsOpen(false)
-      }>close</button>
-      </Modal>
+      
     </>
   );
 }
--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -1,17 +1,48 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 
 function GifList(props) {
 
     const gif_use = props.gif;
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const [selectedItem, setselectedItem] = useState([]);
+
+  
     return (
         <>
-            <div className="img_div">
+            <div className="img_div row">
+                
                 {
-                    gif_use.map((gif_use1, i) => 
-                <img src={gif_use1.images.downsized.url} key={i}/>
+                    gif_use.map((gif_use1, id) => 
+                <img className="col-lg-4" src={gif_use1.images.downsized.url} 
+                alt='gif-result' 
+                onClick={ () => {
+                    setModalIsOpen(true);
+                    setselectedItem(gif_use);
+                    console.log(selectedItem)
+                }}
+                key={id} />
+                
                 )}
             </div>
-        </>
+            <Modal isOpen={modalIsOpen} selectedItem={selectedItem} onRequestClose={
+                () => setModalIsOpen(false)
+                }>
+                     {
+                    selectedItem.map((selectedItem1, id) => 
+                <img src={selectedItem1.images.downsized.url} 
+                alt='gif-result' 
+                key={id} />
+                
+                )}
+                    
+                    <button onClick={
+                    () => setModalIsOpen(false)
+                    }>close</button>
+                </Modal>
+         </>
     );
 }
 
